26d28bc1652d2a5d9a13b2cfd2aabc2d3f5b7715
etar125 <inniesharpsoftware@gmail.com>
2026-01-11 18:13:41 +0300

    Обновлён ftpauto.sh, upload.sh, gitignore

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-[[ -d ".static/.ftpauto" ]] && mv .static/.ftpauto ../
+[[ -d ".static/.ftpauto" ]] && mv .static/.ftpauto ./
 ./e1sg.tcl .
-[[ -d ".ftpauto" ]] mv .ftpauto .static/
+[[ -d ".ftpauto" ]] && mv .ftpauto .static/
 cp ftpauto.sh .static/
 cp .env .static/
 cd .static
