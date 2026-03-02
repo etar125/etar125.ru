e1da3952c9c88a9e4418afc1ee86ebc961a597bf
etar125 <inniesharpsoftware@gmail.com>
2026-03-02 18:18:57 +0300

    Переписан скрипт для генерации блога
    Сайт пока не переделан под него

--- e1sg.tcl
+++ e1sg.tcl
@@ -1,12 +1,12 @@
 #!/bin/env tclsh
-# etar125's site generator v0.26.2_8
+# etar125's site generator v0.26.3_2
 # Copyright (c) 2025-2026 etar125
 # 
 # Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
 # 
 # THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-set version "0.26.2_8"
+set version "0.26.3_2"
 
 proc usage {} {
 	puts "etar125's site generator v$version"
