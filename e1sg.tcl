87d04ff469a97193314faffbbfcdd866a9989bdc
etar125 <inniesharpsoftware@gmail.com>
2026-03-31 14:26:30 +0300

    Обновлён стиль, исправления, обновлён e1sg
    Собран сайт

--- e1sg.tcl
+++ e1sg.tcl
@@ -1,12 +1,12 @@
 #!/bin/env tclsh
-# etar125's site generator v0.26.3_2
+# etar125's site generator v0.26.3_31
 # Copyright (c) 2025-2026 etar125
 # 
 # Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
 # 
 # THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-set version "0.26.3_2"
+set version "0.26.3_31"
 
 proc usage {} {
 	puts "etar125's site generator v$version"
@@ -107,7 +107,7 @@ proc convert_dir {dir} {
 
 convert_dir ""
 
-set html_menu ""
+set html_menu "<div class=\"header\">\n"
 
 # Logo
 
@@ -153,7 +153,7 @@ if {![info exists html_nav_menu]} {
 	</li>\n"
 	}
 } else { append html_menu $html_nav_menu }
-append html_menu "</ul>\n</div>\n"
+append html_menu "</ul>\n</div>\n</div>\n"
 
 set html_body "<!DOCTYPE html>
 <html>
